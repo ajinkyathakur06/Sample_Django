eead521da379161ac99c48459ea64f45d16c17bb
ajinkyathakur06 <thakurajinkya007@gmail.com>
2025-08-21 18:58:08 +0530

    Update settings.py

--- testdeploy/settings.py
+++ testdeploy/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': 'ajinkya',
         'PASSWORD': '2401199',
         'HOST': 'ajinkya-db',  # instead of 127.0.0.1
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
